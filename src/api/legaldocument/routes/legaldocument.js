4e2311382b7d49162bcf96b9a0b5c0cffeb5f887
Luca Kiebel <github@luca-kiebel.de>
2022-07-16 16:04:06 +0200

    first commit

---
+++ src/api/legaldocument/routes/legaldocument.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * legaldocument router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::legaldocument.legaldocument');
---
+++ src/api/legaldocument/services/legaldocument.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * legaldocument service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::legaldocument.legaldocument');
